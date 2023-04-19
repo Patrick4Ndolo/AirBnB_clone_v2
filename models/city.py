b63dd420837b55f3b75e83964470fe1dbd8b11f2
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-04-19 15:34:27 +0300

    correction of test_base_model file

--- models/city.py
+++ models/city.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from od import getenv
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade='all, delete, delete-orphan")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete, delete-orphan")
 
