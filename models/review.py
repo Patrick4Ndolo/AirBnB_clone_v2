d21d3a6b1210333284956430715c5a8291058ee6
Jedidah <jedidahwambuguw@gmail.com>
2023-04-22 20:07:14 +0300

    Corrections to models directory

--- models/review.py
+++ models/review.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
@@ -16,4 +15,3 @@ class Review(BaseModel, Base):
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
-
