29e723295367df2c33e18b0b7551eaa76f41f214
Patrick4Ndolo <ndolopatrick52@gmail.com>
2023-05-21 20:44:22 +0300

    updated files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
+
 """Flask script that displays “Hello HBNB!” """
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_world():
     """a method/function that returns hello HBNB"""
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0", port=5000)
-
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,5 +13,6 @@ def hbnb():
     """The method returns HBNB"""
     return "HBNB"
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0", port=5000)
