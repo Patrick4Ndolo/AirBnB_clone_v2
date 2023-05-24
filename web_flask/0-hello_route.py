22d0d72a404c6f679a6fcd2c9464614fd7079030
Jedidah <jedidahwambuguw@gmail.com>
2023-05-25 00:06:54 +0300

    Added Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-
-
 """Flask script that displays “Hello HBNB!” """
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello():
     """a method/function that returns hello HBNB"""
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
